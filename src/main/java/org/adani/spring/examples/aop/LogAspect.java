6f9fb2ffe10699a1d39259ea5462517f79fe090b
Imran Adan <imranadan@gmail.com>
2016-03-22 22:11:03 +0000

    Added Entry and Exit as part of string message for aspect logging

--- src/main/java/org/adani/spring/examples/aop/LogAspect.java
+++ src/main/java/org/adani/spring/examples/aop/LogAspect.java
@@ -5,34 +5,56 @@ import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ *
+ */
 @Aspect
 public class LogAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LogAspect.class);
 
+    /**
+     *
+     * TODO: Complete Documentation
+     * @param jp
+     */
     @Before("execution(* org.adani.spring.examples.aop.*.*(..)) && within(org.adani.spring.examples.aop.*)")
     public void onEntry(JoinPoint jp) {
         String args = getJointPointArgs(jp);
-        String actionMessage = "ACTION ~> [ " + jp.getSignature().getDeclaringTypeName() + "::" + jp.getSignature().getName() +"(..) -> " + args + "]";
+        String actionMessage = "ENTRY ~> [ " + jp.getSignature().getDeclaringTypeName() + "::" + jp.getSignature().getName() +"(..) -> " + args + "]";
         LOGGER.info(actionMessage);
     }
 
+    /**
+     * TODO: Complete Documentation
+     * @param jp
+     */
     @After("execution(* org.adani.spring.examples.aop.*.*(..)) && within(org.adani.spring.examples.aop.*)")
     public void onExit(JoinPoint jp) {
         String args = getJointPointArgs(jp);
-        String actionMessage = "ACTION ~> [ " + jp.getSignature().getDeclaringTypeName() + "::" + jp.getSignature().getName() +"(..) -> " + args + "]";
+        String actionMessage = "EXIT ~> [ " + jp.getSignature().getDeclaringTypeName() + "::" + jp.getSignature().getName() +"(..) -> " + args + "]";
         LOGGER.info(actionMessage);
     }
 
+    /**
+     * TODO: Complete Documentation
+     * @param jp
+     * @param ex
+     */
     @AfterThrowing(pointcut = "execution(* org.adani.spring.examples.aop.*.*(..)) && within(org.adani.spring.examples.aop.*)", throwing = "ex")
     public void onExceptionThrown(JoinPoint jp, Throwable ex) {
         String fault = "EXCEPTION [ " + ex.getMessage() + " ]";
         LOGGER.info(fault);
         String args = getJointPointArgs(jp);
-        String actionMessage = "ACTION ~> [ " + jp.getSignature().getDeclaringTypeName() + "::" + jp.getSignature().getName() +"(..) ~> " + args + "\n"+fault+"]";
+        String actionMessage = fault + " ~> EXIT ~> [ " + jp.getSignature().getDeclaringTypeName() + "::" + jp.getSignature().getName() +"(..) ~> " + args +"]";
         LOGGER.info(actionMessage);
     }
 
+    /**
+     * TODO: Complete Documentation
+     * @param jp
+     * @return
+     */
     private String getJointPointArgs(JoinPoint jp) {
         String actionArgs = "ARGS: [";
         for (int i = 0; i < jp.getArgs().length; ++i) {
